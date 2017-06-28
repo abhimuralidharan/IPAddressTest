0f4d88523ebf4f18428cd413cb01e88c949d6d95
Abhilash KM <abhimuralidharan1992@gmail.com>
2017-06-28 15:38:28 +0530

    Initial commit

---
+++ ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  IPAddressTest
+//
+//  Created by Apple on 28/06/17.
+//  Copyright © 2017 Abhilash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *ipaddressLabel;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IPAddressTest
+//
+//  Created by Apple on 28/06/17.
+//  Copyright © 2017 Abhilash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
